59dd149d60c1c30197edf11d81371e458ab3277c
Steami-15 <bubbasdeherrera@gmail.com>
2024-11-15 14:06:19 -0700

    Create all the list functions.py

---
+++ all the list functions.py
@@ -0,0 +1,19 @@
+#list for the zoo
+zoo = ["monkey", "elephant", "lion", "tiger", "giraffe"]
+
+#adding panda to the end of the list
+
+zoo.append("panda")
+
+#removing the lion
+zoo.remove("lion")
+
+#replacing elephant with meerkat
+zoo[1] = "meerkat"
+
+#length of zoo printing
+zoo2 = len(zoo)
+
+#print statements for the zoo and length
+print(zoo)
+print(zoo2)
